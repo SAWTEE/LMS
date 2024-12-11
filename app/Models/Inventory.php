856cd951ea33a4abc2b4221175ecb3ef46c3a6e1
Ankur Singh <abishek.singh.chauhan@gmail.com>
2024-12-11 23:06:23 +0000

    Change Stock to price column in inventory

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -15,9 +16,17 @@ class Inventory extends Model
         'item_name',
         'quantity',
         'item_category',
-        'in_stock',
+        'price',
         'remarks',
     ];
+
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => $value / 100,
+            set: fn($value) => $value * 100
+        );
+    }
 
     public function inventoryIssues()
     {
